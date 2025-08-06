307a254fe071b74807c46576b967695521bc3a14
Artyom <art161994@yandex.ru>
2025-08-06 11:25:44 +0200

    feat: add useLazyGetPokemonByNameQuery hook to PokemonDeatil component, and update it

--- class-components/my-app/src/components/pokemonDetail/PokemonDetail.tsx
+++ class-components/my-app/src/components/pokemonDetail/PokemonDetail.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import {
-  useGetPokemonByNameQuery,
   useLazyGetPokemonByNameQuery,
 } from '../../api/pokemonApi';
 import './PokemonDetail.css';
@@ -10,47 +9,41 @@ const PokemonDetail: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const detailsId = searchParams.get('details');
 
-  const {
-    data: pokemon,
-    isLoading,
-    isError,
-    refetch,
-  } = useGetPokemonByNameQuery(detailsId!, {
-    skip: !detailsId,
-  });
+  const [
+    triggerFetch,
+    { data: pokemon, isLoading, isError },
+  ] = useLazyGetPokemonByNameQuery();
 
-  const [trigger, { isFetching }] = useLazyGetPokemonByNameQuery();
+  useEffect(() => {
+    if (detailsId) {
+      triggerFetch(detailsId);
+    }
+  }, [detailsId, triggerFetch]);
 
   const handleClose = () => {
     searchParams.delete('details');
     setSearchParams(searchParams);
   };
 
-  const handleRefresh = () => {
-    if (detailsId) {
-      trigger(detailsId);
-    }
-  };
-
   if (!detailsId) return null;
 
   return (
     <div className="pokemon-detail">
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <button className="pokemon-detail__close-button" onClick={handleClose}>
-          Close
-        </button>
-        <button className="pokemon-detail__close-button" onClick={handleRefresh}>
-          Refresh
-        </button>
-      </div>
-
-      {(isLoading || isFetching) && <p>Loading details...</p>}
+      <button
+        className="pokemon-detail__close-button"
+        onClick={handleClose}
+      >
+        Close
+      </button>
+      {isLoading && <p>Loading details...</p>}
       {isError && <p>Pokemon not found</p>}
       {pokemon && (
         <div className="pokemon-detail__info">
           <h2>{pokemon.name}</h2>
-          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+          />
           <p>Height: {pokemon.height}</p>
           <p>Weight: {pokemon.weight}</p>
         </div>
