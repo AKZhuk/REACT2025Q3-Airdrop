e4fcff03e4e803424af535eb6cee49ab44ab1892
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2025-08-20 14:10:34 +0300

    fix i18n

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,8 +1,6 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-  let locale = await requestLocale;
-
+export default getRequestConfig(async ({ locale }) => {
   return {
     messages: (await import(`./messages/${locale}.json`)).default
   };
